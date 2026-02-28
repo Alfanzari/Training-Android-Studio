075fc4e65b64dec13e8cf35bd0e873c332b7b930
syauqialfanzari <alfanzarisyauqi@gmail.com>
2026-02-27 21:45:42 -0800

    Menambahkan TopBar PART 1

--- app/src/main/java/com/syauqialfanzari0008/training1/MainActivity.kt
+++ app/src/main/java/com/syauqialfanzari0008/training1/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.syauqialfanzari0008.training1
 
+import android.content.res.Configuration // Tambahkan ini untuk Preview Night Mode
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.syauqialfanzari0008.training1.ui.theme.Training1Theme
 
@@ -19,29 +24,50 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Training1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // PERUBAHAN 1: Memanggil MainScreen, bukan Scaffold/Greeting lama
+                MainScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        // PERUBAHAN 2: Menghubungkan ke ScreenContent dengan padding yang benar
+        ScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello Android!",
         modifier = modifier
     )
 }
 
+// PERUBAHAN 3: Update Preview agar menampilkan MainScreen, bukan Greeting
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true) // Tambahan untuk tema gelap
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     Training1Theme {
-        Greeting("Android")
+        MainScreen()
     }
 }
+
+// Catatan: Fungsi Greeting sudah tidak dipakai lagi menurut tutorial, bisa dihapus.
